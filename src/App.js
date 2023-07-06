49e7847beb14eaf1c273fc0722f23a502255e5d8
unknown <biloldadamirzayev3@gmail.com>
2023-07-05 23:28:24 -0700

    tshgww

--- src/App.js
+++ src/App.js
@@ -1,16 +1,4 @@
-<<<<<<< HEAD
-HEAD
-import React from 'react';
-const App = () => {
-    return (
-        <div className='App'>
-        </div>
-    );
-
-
-
-=======
->>>>>>> e40383481b9f285032f4b6e7e81e15d41099bbf7
+git 
 import React from "react";
 import Xojiakbar from "./Xojiakbar/xojiakbar";
 
@@ -25,7 +13,7 @@ function App() {
 
     </div>
   );
->>>>>>> f59215d99336a3f0539fb785f6af1ac5807e0a3a
+
 }
 
 export default App;
