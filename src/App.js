f31d7f5cea89be72e985d2bef2511371b8700c3d
unknown <nurbekasadillaev777@gmail.com>
2023-07-06 11:32:27 +0500

    salom strat123456

--- src/App.js
+++ src/App.js
@@ -1,45 +1,33 @@
-<<<<<<< HEAD
+
 import React from "react"
-import { Nurbek } from "./pages/Nurbek/Nurbek";
+// import { Nurbek } from "./pages/Nurbek/Nurbek";
 
 
   
-=======
-<<<<<<< HEAD
-HEAD
-import React from 'react';
-const App = () => {
-    return (
-        <div className='App'>
-        </div>
-    );
 
 
 
-=======
->>>>>>> e40383481b9f285032f4b6e7e81e15d41099bbf7
-import React from "react";
-import Xojiakbar from "./Xojiakbar/xojiakbar";
 
-import { Shop } from "./pages/Shop/Shop";
->>>>>>> c573c9006de6f9dfbe7aca7064bf222c84d29cb1
+
+
+
+
+
+
 
   function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
+
    {/* salom start */}
 
-   <Nurbek/>
-=======
-    <Xojiakbar></Xojiakbar>
+   {/* <Nurbek/> */}
 
-      <Shop/>
+  
 
->>>>>>> c573c9006de6f9dfbe7aca7064bf222c84d29cb1
     </div>
   );
->>>>>>> f59215d99336a3f0539fb785f6af1ac5807e0a3a
+
 }
 
 export default App;
