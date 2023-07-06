7ab1efc8043efc0e6e01a6b1d5d61b9677063a61
xojiakbar123121321 <xojiakbarisoqjanov9@gmail.com>
2023-07-06 10:46:47 +0500

    salom

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-
-
 import React from "react";
+import Xojiakbar from "./Xojiakbar/xojiakbar";
 
 function App() {
   return (
     <div className="App">
-   salom start
+    <Xojiakbar></Xojiakbar>
     </div>
   );
 }
